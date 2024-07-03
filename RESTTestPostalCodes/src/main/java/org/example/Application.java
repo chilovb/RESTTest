9d4b176ad5c0301c47c83b455457919330605dd9
chilovb <yah96chiva@yahoo.co.uk>
2024-07-03 11:56:01 +0200

    Delete RESTTestPostalCodes directory

--- RESTTestPostalCodes/src/main/java/org/example/Application.java
+++
@@ -1,9 +0,0 @@
-package org.example;
-import org.springframework.boot.SpringBootApplication;
-@SpringBootApplication
-public class Application {
-
-    public static void main(String[] args) {
-        SpringApplication.run(Application.class, args);
-    }
-}
--- RESTTestPostalCodes/src/main/java/org/example/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package org.example;
-
-public class ProductNotFoundException extends RuntimeException {
-
-    public ProductNotFoundException(Long id) {
-        super("Product with ID " + id + " not found.");
-    }
-}
