c8e400f881d4711a142992085a1f7066d127c424
chilovb <yah96chiva@yahoo.co.uk>
2024-07-03 09:58:37 +0200

    init

--- RESTTestPostalCodes/src/main/java/org/example/ProductRepository.java
+++ RESTTestPostalCodes/src/main/java/org/example/ProductRepository.java
@@ -1,4 +1,10 @@
 package org.example;
 
+import java.util.List;
+
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    @Query(value = "SELECT * FROM countries WHERE code = :countryCode", nativeQuery = true)
+    List<Product> findCountryByCountryCode(@Param("countryCode") String countryCode);
+
 }
